22e8dd2eace99aac22583db4912a2cacb239dda5
Gcastro <geraldocastro36@gmail.com>
2025-01-23 22:54:42 -0800

    "Updated"

---
+++ 211/lab01_add/add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int value1, value2, sum;
+    cin >> value1;
+    cin >> value2;
+
+    sum  = value1 + value2;
+    cout << value1 << " + " << value2 << " = " << sum << endl;
+
+    return 0;
+}
---
+++ 211/lab01_print/print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int x;
+    cin >> x;
+
+    for(int i = 0; i < x; i++) {
+        cout << i << " hello" << endl;
+    }
+
+    return 0;
+}
