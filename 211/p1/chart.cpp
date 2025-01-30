999cfaef53c797c1de96236f042b5e35f1d499d2
Gcastro <geraldocastro36@gmail.com>
2025-01-29 21:09:44 -0800

    "Updated 1/29"

--- 211/p1/chart.cpp
+++ 211/p1/chart.cpp
@@ -5,16 +5,18 @@
 
 #include <iostream>
 
+int maxvalue(int values[], int count);
+
 using namespace std;
 
 int main() {
-    
-    // Sets the max value of 100 
+
+// Sets the max value of 100 
     const int MAX = 100;
     int values[MAX];
     int count = 0;
 
-    // Read input values
+// Read input values
     int value = 0;
     while (count < MAX) {
         cin >> value;
@@ -25,15 +27,9 @@ int main() {
         count++;
     }
 
-    // Find the maximum value
-    int max_value = 0;
-    for (int i = 0; i < count; i++) {
-        if (values[i] > max_value) {
-            max_value = values[i];
-        }
-    }
+    int max_value = maxvalue(values, count);
 
-    // Print the bar chart
+// Print the bar chart
     for (int i = max_value; i > 0; i--) {
         for (int j = 0; j < count; j++) {
             if (values[j] >= i) {
@@ -47,4 +43,15 @@ int main() {
     }
 
     return 0;
+}
+
+// Find the maximum value
+int maxvalue(int values[], int count) {
+    int max_value = 0;
+    for (int i = 0; i < count; i++) {
+        if (values[i] > max_value) {
+            max_value = values[i];
+        }
+    }
+    return max_value;
 }
