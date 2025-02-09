c331f7cb2ce9c03dd1087efb4a6ab26ff37413fd
Gcastro <geraldocastro36@gmail.com>
2025-02-08 20:46:50 -0800

    " "

--- 211/lab03_video/video.cpp
+++ 211/lab03_video/video.cpp
@@ -6,9 +6,5 @@ Video::Video(string t, string u, string c, double l, int r)
     : title(t), url(u), comment(c), length(l), rating(r) {}
 
 void Video::print() {
-    cout << "Title: " << title << "\n";
-    cout << "URL: " << url << "\n";
-    cout << "Comment: " << comment << "\n";
-    cout << "Length: " << length << "\n";
-    cout << "Rating: " << rating << "\n";
+    cout << title << ", " << url << ", " << comment << ", " << length << ", " << rating << "\n";
 }
