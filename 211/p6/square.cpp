3912f420e31d7281503f657f8a127518fdcdc653
Gcastro <geraldocastro36@gmail.com>
2025-04-26 18:32:46 -0700

    " "

--- 211/p6/square.cpp
+++ 211/p6/square.cpp
@@ -6,7 +6,10 @@ Square::Square(int x, int y, int size) : Shape(x, y), m_size(size) {}
 void Square::draw(Grid &grid) {
     for (int i = 0; i < m_size; ++i) {
         for (int j = 0; j < m_size; ++j) {
-            grid.set(m_x + i, m_y + j, '*'); // Use m_x and m_y from the Shape base class
+            // Draw the border of the square
+            if (i == 0 || i == m_size - 1 || j == 0 || j == m_size - 1) {
+                grid.set(m_x + i, m_y + j, '*');
+            }
         }
     }
 }
--- 211/p6/triangle.cpp
+++ 211/p6/triangle.cpp
@@ -4,11 +4,13 @@
 Triangle::Triangle(int x, int y) : Shape(x, y) {}
 
 void Triangle::draw(Grid &grid) {
-    // Drawing a simple triangle shape on the grid
-    grid.set(m_x, m_y, '+');         // Top point
-    grid.set(m_x - 1, m_y + 1, '+'); // Left point
-    grid.set(m_x + 1, m_y + 1, '+'); // Right point
-    grid.set(m_x, m_y + 2, '+');     // Middle bottom point
-    grid.set(m_x - 1, m_y + 2, '+'); // Bottom left point
-    grid.set(m_x + 1, m_y + 2, '+'); // Bottom right point
+    // Shifting the triangle one space to the right
+    grid.set(m_x + 2, m_y, '+');         // Top point
+    grid.set(m_x + 1, m_y + 1, '+');     // Left point
+    grid.set(m_x + 3, m_y + 1, '+');     // Right point
+    grid.set(m_x, m_y + 2, '+');         // Bottom left point
+    grid.set(m_x + 1, m_y + 2, '+');     // Bottom middle-left point
+    grid.set(m_x + 2, m_y + 2, '+');     // Bottom middle point
+    grid.set(m_x + 3, m_y + 2, '+');     // Bottom middle-right point
+    grid.set(m_x + 4, m_y + 2, '+');     // Bottom right point
 }
