09350837595202bcdc52b4cf2c5a5aad8c6a088f
Gcastro <geraldocastro36@gmail.com>
2025-10-28 10:31:38 -0700

    " "

--- 311/lab6/HashTable.cpp
+++ 311/lab6/HashTable.cpp
@@ -1,20 +1,29 @@
-// CSCI 311 - Spring 2023
+// CSCI 311
 // Lab 6 - Hash Table cpp
-// Author: Carter Tillquist
+// Author: Geraldo Castro
 
 #include <iostream>
 #include <math.h>
 #include "HashTable.h"
 
 HashTable::HashTable(){
+  // defaults as expected by driver/tests
+  size = 11;
+  numElements = 0;
+  p = 31;
+  table = std::vector<std::vector<std::string>>(size);
 }
 
 HashTable::HashTable(int s, int mult){
+  size = s;
+  p = mult;
+  numElements = 0;
+  table = std::vector<std::vector<std::string>>(size);
 }
 
-int HashTable::getSize(){ return -1; }
-int HashTable::getNumElements(){ return -1; }
-int HashTable::getP(){ return -1; }
+int HashTable::getSize(){ return size; }
+int HashTable::getNumElements(){ return numElements; }
+int HashTable::getP(){ return p; }
 
 void HashTable::printTable(){
   std::cout << "HASH TABLE CONTENTS" << std::endl;
@@ -30,18 +39,56 @@ void HashTable::printTable(){
 }
 
 int HashTable::search(std::string s){
+  if (size <= 0) return -1;
+  int idx = hash(s);
+  for (int i = 0; i < (int)table[idx].size(); ++i){
+    if (table[idx][i] == s) return idx;
+  }
   return -1;
 }
 
 void HashTable::insert(std::string s){
+  if (size <= 0) return;
+  int idx = hash(s);
+  table[idx].push_back(s);
+  numElements++;
 }
-
 void HashTable::remove(std::string s){
+  if (size <= 0) return;
+  int idx = hash(s);
+  for (int i = 0; i < (int)table[idx].size(); ++i){
+    if (table[idx][i] == s){
+      table[idx].erase(table[idx].begin() + i);
+      numElements--;
+      return;
+    }
+  }
+
 }
 
 void HashTable::resize(int s){
+  if (s <= 0) return;
+  // save old contents
+  std::vector<std::vector<std::string>> old = table;
+  // set new size and clear table
+  size = s;
+  table = std::vector<std::vector<std::string>>(size);
+  // re-insert all elements (preserves insertion order per old buckets)
+  int oldNum = numElements;
+  numElements = 0;
+  for (int i = 0; i < (int)old.size(); ++i){
+    for (int j = 0; j < (int)old[i].size(); ++j){
+      insert(old[i][j]);
+    }
+  }
+  (void)oldNum;
 }
 
 int HashTable::hash(std::string s){
-  return -1;
+  if (size <= 0) return -1;
+  long long h = 0;
+  for (char c : s){
+    h = (h * p + static_cast<unsigned char>(c)) % size;
+  }
+  return static_cast<int>(h);
 }
