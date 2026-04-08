6447043350411c3dd46ea1b0ff85db70a38e8c12
Gcastro <geraldocastro36@gmail.com>
2026-04-07 22:47:25 -0700

    "   "

--- job_copilot/app/ai_client.py
+++ job_copilot/app/ai_client.py
@@ -1,4 +1,9 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ.get("Sn3k"))
+api_key = os.environ.get("Sn3k")
+
+if not api_key:
+    raise ValueError("Sn3k is not set")
+
+client = OpenAI(api_key=api_key)
