0fcbfb7ed2cbbdcf18500afcfab26f454f4f8499
Gcastro <geraldocastro36@gmail.com>
2026-04-07 23:02:03 -0700

    "OPENAI_API_KEY is not set" --- IGNORE ---

--- job_copilot/app/ai_client.py
+++ job_copilot/app/ai_client.py
@@ -1,9 +1,9 @@
 import os
 from openai import OpenAI
 
-api_key = os.environ.get("Sn3k")
+api_key = os.environ.get("OPENAI_API_KEY")
 
 if not api_key:
-    raise ValueError("Sn3k is not set")
+    raise ValueError("OPENAI_API_KEY is not set")
 
 client = OpenAI(api_key=api_key)
